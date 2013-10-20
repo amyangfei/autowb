ee0605d250325c76ba1caa032f1371780fae2e8d
amyangfei <amyangfei@gmail.com>
2013-10-20 13:52:04 +0800

    fix None Dict bug

--- code/autowb/account/forms.py
+++ code/autowb/account/forms.py
@@ -7,11 +7,12 @@ from weibo import APIClient
 
 
 def _get_priority_data(access_token):
+    if not access_token:
+        return {'access_token': None, 'expires_in': None, 'uid': None}
     client = APIClient('', '')
     client.access_token = access_token
     try:
         res = client.get_token_info(access_token)
-        print res
         expire_time = res.get('expire_in', '') + res.get('create_at', '')
         expires_in = datetime.datetime.fromtimestamp(expire_time)
         uid = res.get('uid')
@@ -34,17 +35,18 @@ class PriorityTokenForm(forms.Form):
     )
 
     def __init__(self, user, *a, **kw):
-        # super(UserEditForm, self).__init__(*a, **kw)
         priority_data = user.get_priority_data()
         super(PriorityTokenForm, self).__init__(initial={
             'priority_access_token': priority_data.get('access_token'),
             'expires_in': priority_data.get('expires_in')}, *a, **kw)
+        self.priority_data = {}
 
     def clean(self):
         cd = super(PriorityTokenForm, self).clean()
         errors = []
         # validation of access_token
         priority_data = _get_priority_data(cd.get('priority_access_token'))
+        self.priority_data = priority_data
         if not priority_data:
             errors.append(u'错误的或过期的access_token')
         if errors:
@@ -52,5 +54,5 @@ class PriorityTokenForm(forms.Form):
         return cd
 
     def save(self, user):
-        priority_data = _get_priority_data(self.cleaned_data.get('priority_access_token'))
+        priority_data = self.priority_data
         user.update_priority_data(**priority_data)
